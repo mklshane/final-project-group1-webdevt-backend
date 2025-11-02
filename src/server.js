b992c91b3a348ecee9b7ee4b3e3be3e164b261a5
Shane Estrellanes <mikaela_shane_estrellanes@dlsl.edu.ph>
2025-11-02 22:54:01 +0800

    Update server.js

--- src/server.js
+++ src/server.js
@@ -20,7 +20,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // CORS
-const allowedOrigins = ["http://localhost:5173", "http://localhost:5174", "medisys.vercel.app"];
+const allowedOrigins = ["http://localhost:5173", "http://localhost:5174", "https://medisys.vercel.app"];
 app.use(
   cors({
     origin: allowedOrigins,
