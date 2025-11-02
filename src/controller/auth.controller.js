650218a470addc4cf480e7b10cd2db19c14eca25
Shane Estrellanes <mikaela_shane_estrellanes@dlsl.edu.ph>
2025-11-03 03:34:47 +0800

    Update auth.controller.js

--- src/controller/auth.controller.js
+++ src/controller/auth.controller.js
@@ -201,8 +201,8 @@ export const logout = (req, res) => {
   try {
     res.clearCookie("token", {
       httpOnly: true,
-      secure: false, // set to true in production
-      sameSite: "strict",
+      secure: true, // set to true in production
+      sameSite: "none",
     });
 
     return res.status(200).json({ message: "Logged out successfully" });
